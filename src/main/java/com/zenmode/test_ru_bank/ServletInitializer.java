60ffabb8ecca40907ce43f3376a80faa8b0056e7
Marina Kaygorodova <kajgorodovamarina@gmail.com>
2020-05-14 13:14:53 +0200

    Init

---
+++ src/main/java/com/zenmode/test_ru_bank/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zenmode.test_ru_bank;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestRuBankApplication.class);
+	}
+
+}
